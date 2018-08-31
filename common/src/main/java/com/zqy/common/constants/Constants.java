d954fc4292b978e70e711116dd542e950eea9c6d
ningfangbing <ningfangbing@sinochem.com>
2018-08-31 13:24:33 +0800

    common 项目

---
+++ common/src/main/java/com/zqy/common/constants/Constants.java
@@ -0,0 +1,20 @@
+package com.zqy.common.constants;
+
+/**
+ * @Description: 系统常量
+ * @TODO:
+ * @Author: nfbing
+ * @Date: 2018/5/5 11:01
+ * @version: V1.0
+ */
+public class Constants {
+    /**
+     * 语言类型-中文
+     */
+    public static final Integer LANG_ZH = 0;
+
+    /**
+     * 语言类型-英文
+     */
+    public static final Integer LANG_EN = 1;
+}
